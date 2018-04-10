adcc7d3b22150e83ecc08826bc83db086a3fdfec
Nikita <nikita@MacBook-Pro-Nikita.local>
2018-04-10 14:35:00 +0300

    initial commit in new repo

---
+++ tests/header.test.js
@@ -0,0 +1,34 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+test('We can launch a browser', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  await page.login();
+
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
+
+  expect(text).toEqual('Logout');
+});
